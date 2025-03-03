e4606f10b9c8987b2d1b008e0e37d994d1008c65
Sahil Naik <nsahil992@gmail.com>
2025-03-03 16:52:45 +0530

    feat: PostgreSQL

---
+++ schema.sql
@@ -0,0 +1,23 @@
+-- Create friends table
+CREATE TABLE IF NOT EXISTS friends (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+-- Create items table
+CREATE TABLE IF NOT EXISTS items (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    friend_id INTEGER REFERENCES friends(id)
+);
+
+-- Add some sample data
+INSERT INTO friends (name) VALUES ('Courage'), ('Doraemon'), ('Ben');
+INSERT INTO items (name, friend_id) VALUES 
+('Computer', (SELECT id FROM friends WHERE name = 'Doraemon')),
+('Gadget', (SELECT id FROM friends WHERE name = 'Doraemon')),
+('Watch', (SELECT id FROM friends WHERE name = 'Ben'));
+
+SELECT f.id AS friend_id, f.name AS friend_name, i.id AS item_id, i.name AS item_name
+FROM friends f
+LEFT JOIN items i ON f.id = i.friend_id;
